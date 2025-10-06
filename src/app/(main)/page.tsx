e34c5aa28c0d35beee6f0cb3e5e5b3788fd5cc87
Khan <alshujah@gmail.com>
2025-10-06 09:17:49 +0000

    I m getting page nor found error while opening app

--- src/app/(main)/page.tsx
+++
@@ -1,5 +0,0 @@
-
-// This page is intentionally left blank to resolve a routing conflict.
-// The root page is now handled by /src/app/page.tsx.
-// The main dashboard is at /src/app/(main)/dashboard/page.tsx.
-export default () => null;
